72fe9ee3a6765bc38c1f09a0309983ca095d0b7d
Jeff Wilkey <contact@jeffwilkey.com>
2023-07-28 03:12:25 -0700

    feat: define todo CRUD and associated tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeffwilkey/todo-cli
+
+go 1.20
---
+++ todo.go
@@ -0,0 +1,86 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type List []item
+
+// Method to add a new item to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
+
+// Complete method marks a todo item as completed
+// by setting the Done field to true and CompletedAt to the current time
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i < 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+
+	// Adjust index for 0 based index
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+// Delete method removes an item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+
+	// Adjust index for 0 based index
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+}
+
+// Save method encodes the List as JSON and saves it
+// using the provided file name
+func (l *List) Save(filename string) error {
+	jsonData, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, jsonData, 0o644)
+}
+
+// Get method decodes and returns a List from a JSON file
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
+}
